72e3e55755f7b656a12d6f03e832bb3c0c38ac66
if17 <if17@A406-11.INFORMAATIKA>
2018-02-01 10:01:15 +0200

    toasoojuse programm

--- Algus/src/algus/Algus.java
+++ Algus/src/algus/Algus.java
@@ -7,15 +7,29 @@ package algus;
 
 /**
  * Esimene katsetus, muutujad, valik, massiv
- * @author if17
  */
-
-public class Algus {
-    public static void ilmaKommentaar(int temperatuur){
-        if(temperatuur>0){System.out.println("Sulab");}
-        else{System.out.println("Jäätab");}
-    }
     
+
+
+     /**
+      * Arbamus ilma kohta
+      * @author if17
+      */
+    public class Algus {
+        public static void ilmaKommentaar(int temperatuur){
+            if(temperatuur>0){System.out.println("Sulab");}
+            else{System.out.println("Jäätab");}
+        }
+    
+    /**
+     * Alamprogramm toa soojuse hindamiseks vastavalt temperatuurile.
+     * vähemalt kolm taset
+     */
+         public static void toaSoojus(int soojus){
+            if(soojus>=25){System.out.println("Liiga soe");}
+                else if(soojus<18){System.out.println("Liiga külm");}
+                    else{System.out.println("Hea Temperatuur");}
+        }
     /**
      * Programmi töö alguskoht
      * @param args the command line arguments
@@ -23,6 +37,7 @@ public class Algus {
     public static void main(String[] args) {
         System.out.println("Hello, Web!");
         ilmaKommentaar(3);
+        toaSoojus(25);
     }
     
 }
